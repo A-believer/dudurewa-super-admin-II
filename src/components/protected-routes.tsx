15b120bdbc0a002117906090e1ef9c5854a3b333
David Abolade <69032300+A-believer@users.noreply.github.com>
2024-01-17 08:34:12 +0100

    push

--- src/components/protected-routes.tsx
+++ src/components/protected-routes.tsx
@@ -1,14 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useAuth } from '@/lib/context/AuthContext'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function ProtectedRouteWrapper({ children }: { children: React.ReactNode }) {
     const { user } = useAuth()
     const navigate = useNavigate()
     
-    if (!user) {
+    useEffect(() => {
+       if (!user) {
         navigate('/auth/login')
-    }
+    } 
+    }, [])
+    
 
     return (
         <>{children}</>
